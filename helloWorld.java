978a3183ad517193ffc64b866ad33842b1d2ca23
Luan Carlos <luan.k28@hotmail.com>
2025-03-15 12:00:37 -0300

    beecrowd 1000

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,7 @@
-import java.io.IOException;
 
 public class helloWorld {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println("Hello World!");
     }
