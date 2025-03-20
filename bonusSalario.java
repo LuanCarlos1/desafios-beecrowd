f662b9f49eccf870b8bc95ebff4b7fb16115e019
Luan Carlos <luan.k28@hotmail.com>
2025-03-20 19:55:45 -0300

    beecrowd 1009

---
+++ bonusSalario.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class bonusSalario {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String name = sc.nextLine();
+        double salary = sc.nextDouble();
+        double sales = sc.nextDouble();
+
+        double bonus = sales * 0.15;
+        double total =  salary + bonus;
+
+        System.out.printf("TOTAL = R$ %.2f\n", total);
+
+        sc.close();
+    }
+}
