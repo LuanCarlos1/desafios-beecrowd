efdf32ffcfc0ca8f28e80c62958959758aa0fe45
Luan Carlos <luan.k28@hotmail.com>
2025-03-21 20:12:28 -0300

    beecrowd 1011

---
+++ esfera.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class esfera {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double ray = input.nextDouble();
+
+        double volume = ((4.0 / 3) * 3.14159 * Math.pow(ray,3));
+
+        System.out.printf("VOLUME = %.3f\n", volume);
+
+        input.close();
+    }
+}
