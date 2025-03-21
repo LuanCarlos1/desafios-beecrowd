4d70b1d3b27e0057d486b325885dd456fbdd8cf0
Luan Carlos <luan.k28@hotmail.com>
2025-03-21 20:12:19 -0300

    beecrowd 1010

---
+++ calculoSimples.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class calculoSimples {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int codPart1 = sc.nextInt();
+        int numPart1 = sc.nextInt();
+        double pricePart1 = sc.nextDouble();
+
+        int codPart2 = sc.nextInt();
+        int numPart2 = sc.nextInt();
+        double pricePart2 = sc.nextDouble();
+
+        double total = (numPart1 * pricePart1) + (numPart2 * pricePart2);
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f\n", total);
+
+
+        sc.close();
+    }
+}
