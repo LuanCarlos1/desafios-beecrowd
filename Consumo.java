076adefb1f2685615b6cdadaed3832a8bab6a24c
Luan Carlos <luan.k28@hotmail.com>
2025-03-30 11:29:15 -0300

    beecrowd 1014

--- Consumo.java
+++ Consumo.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Consumo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int distanciaPercorrida = sc.nextInt();
+        double totalGasto = sc.nextDouble();
+
+        double consumo = distanciaPercorrida/totalGasto;
+
+        System.out.printf("%.3f km/l\n", consumo);
+
+        sc.close();
+    }
 }
