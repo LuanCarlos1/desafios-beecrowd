f615fd831f9cb3d683c6a44f165adc68f8a733b8
Luan Carlos <luan.k28@hotmail.com>
2025-03-30 12:11:48 -0300

    beecrowd 1016

--- Distancia.java
+++ Distancia.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Distancia {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int distanciaKm = sc.nextInt();
+        int tempo = distanciaKm * 2;
+
+        System.out.println(tempo + " minutos");
+
+        sc.close();
+    }
 }
