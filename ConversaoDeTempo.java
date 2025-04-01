00ab71b962687afb9853922408d78fcda09de9c8
Luan Carlos <luan.k28@hotmail.com>
2025-03-31 21:24:59 -0300

    beecrowd 1019

--- ConversaoDeTempo.java
+++ ConversaoDeTempo.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class ConversaoDeTempo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int tempoGasto = sc.nextInt();
+
+        int horas, minutos, segundos;
+
+        horas = tempoGasto / 3600;
+        minutos = (tempoGasto % 3600) / 60;
+        segundos = tempoGasto % 60;
+
+        System.out.println(horas + ":" + minutos + ":" + segundos);
+
+        sc.close();
+    }
 }
