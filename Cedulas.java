e871413e06ed71e42b3686e6492b7f7e2c7d081f
Luan Carlos <luan.k28@hotmail.com>
2025-03-31 21:16:07 -0300

    beecrowd 1018

--- Cedulas.java
+++ Cedulas.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Cedulas {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int valorTotal = sc.nextInt();
+        System.out.println(valorTotal);
+
+        int[] notas = {100, 50, 20, 10, 5, 2, 1};
+
+        for (int nota : notas) {
+            int quantidade = valorTotal / nota;
+            System.out.println(quantidade + " nota(s) de R$ " + nota + ",00");
+            valorTotal %= nota;
+        }
+
+        sc.close();
+
+    }
 }
