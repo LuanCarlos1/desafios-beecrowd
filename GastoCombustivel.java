a000700fa01b55bae8f29b7e2a9a0e387be2acfc
Luan Carlos <luan.k28@hotmail.com>
2025-03-31 21:04:47 -0300

    beecrowd 1017

--- GastoCombustivel.java
+++ GastoCombustivel.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class GastoCombustivel {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int tempoGasto = sc.nextInt();
+        int velocidadeMedia = sc.nextInt();
+
+        int kmPercorrido = tempoGasto * velocidadeMedia;
+
+        double litrosGastos = kmPercorrido / 12.0;
+
+        System.out.printf("%.3f\n", litrosGastos);
+
+        sc.close();
+    }
 }
