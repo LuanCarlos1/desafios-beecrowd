a475fb71275587004d9837c1fa80d0c4640b9122
Luan Carlos <luan.k28@hotmail.com>
2025-04-02 22:07:05 -0300

    beecrowd 1020

--- IdadeEmDias.java
+++ IdadeEmDias.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class IdadeEmDias {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int vida = sc.nextInt();
+        int anosDeVida = vida / 365;
+        vida = vida % 365;
+        int mesesDeVida = vida / 30;
+        vida = vida % 30;
+
+        System.out.println(anosDeVida + " ano(s)");
+        System.out.println(mesesDeVida + " mes(es)");
+        System.out.println(vida + " dia(s)");
+
+        sc.close();
+    }
 }
